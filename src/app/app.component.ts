7db6afa14774423413fabd05bf799aa86a77e8b4
Yuri Cherenev <ucherenev@dexsys.ru>
2018-02-24 22:55:30 +0400

    add basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    // const connection = new HubConnection(`http://localhost:5000/message`);
+
+    // connection.on('Send', msg => {
+    //   console.log('message: ', msg);
+    // });
+
+    // connection.start().then(() => { console.log('Connected'); });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { WatcherComponent } from './watcher/watcher.component';
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    WatcherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'watcher', component: WatcherComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
